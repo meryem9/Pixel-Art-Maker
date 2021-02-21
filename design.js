edb4ecad15feb761614e5227390e07a71fe9b704
meryem9 <79316701+meryem9@users.noreply.github.com>
2021-02-21 20:17:45 +0300

    Add files via upload

--- design.js
+++ design.js
@@ -14,17 +14,13 @@ $('#sizePicker').submit(function(event) {
 
 function makeGrid(height, width) {
 
-  for(let i = 0; i < height; i++)
-    let row = table.insertRow(i);
-  for(let j = 0; j < weight; j++) {
+  for(let i = 0; i < inputHeight.value; i++) {
+    let row = tableElement.insertRow(i);
+  for(let j = 0; j < inputWidth; j++) {
     let cell = row.insertCell(j);
     cell.addEventListener("click", function(event) {
       cell.style.backgroundColor = color.value;
-    });
-  }
-
-  }
-
-// Your code goes here!
-
+     });
+   }
+ }
 }
