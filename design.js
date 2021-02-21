e18c2dea0960f09f88007d099005a6be8e9bd9b3
meryem9 <79316701+meryem9@users.noreply.github.com>
2021-02-21 20:54:38 +0300

    Add files via upload

--- design.js
+++ design.js
@@ -8,19 +8,28 @@ const inputWidth = document.getElementById("inputWidth");
 
 $('#sizePicker').submit(function(event) {
   event.preventDefault();
-  makeGrid(height, width);
+  makeGrid(inputHeight, inputWidth);
 } )
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(height, width) {
+function makeGrid(inputHeight, inputWidth) {
 
   for(let i = 0; i < inputHeight.value; i++) {
     let row = tableElement.insertRow(i);
-  for(let j = 0; j < inputWidth; j++) {
+  for(let j = 0; j < inputWidth.value; j++) {
     let cell = row.insertCell(j);
     cell.addEventListener("click", function(event) {
       cell.style.backgroundColor = color.value;
      });
    }
  }
+}
+function addColor () {
+  let coloredCell = document.querySelector('colorful')
+  for ( var o = 0; x < coloredCell.length; x++) {
+    let color = document.getElementById('#colorPicker');
+    addColor.addEventListener(click, function(event) {
+      event.target.style.backgroundColor = color.value;
+    });
+  }
 }
